9aaa79bf4d7e8a4c39745637edf1e78ae4c9c93c
Plochem <plochemisswag@gmail.com>
2018-04-12 19:24:58 -0700

    Changed color
    cuz it looks better lol

--- src/com/plochem/tos/game/items/MafiaChooser.java
+++ src/com/plochem/tos/game/items/MafiaChooser.java
@@ -8,7 +8,7 @@ public class MafiaChooser extends GameItem{
 	public MafiaChooser(){
 		this.setItemRep(new ItemStack(Material.IRON_SWORD));
 		this.setNameOfItem("§cChoose a player to kill!");
-		this.setOnClickMsg("§cYou decided to kill ");
+		this.setOnClickMsg("§eYou decided to kill ");
 		this.setMenuName("Choose a player to kill!");
 	}
 }
