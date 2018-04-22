fc4d80160f0c2f205dd0a5e5efdac593ed42d568
Plochem <plochemisswag@gmail.com>
2018-04-22 16:43:54 -0700

    Fixed choosing yourself

--- src/com/plochem/tos/listeners/InventoryClick.java
+++ src/com/plochem/tos/listeners/InventoryClick.java
@@ -29,19 +29,20 @@ public class InventoryClick implements Listener{
 				String selectedPlayerName = e.getCurrentItem().getItemMeta().getDisplayName();
 				if(selectedPlayerName.equals(p.getName())){ //TODO allow to click itself if doctor or vig/vet (whichever has the alerts)
 					p.sendMessage("§cYou cannot do that to yourself!"); //TODO fix message printing that you can kill yourself
-				}
-				for(GameItem items : gp.getItems()){
-					if(items.getGUI().equals(e.getClickedInventory())){ //check if player is clicking in the GUI designated for its role
-						if(gp.getArena().getGame().getEvent().equals("Night")){  //TODO allow for jailors to do their choosing
-							p.sendMessage(items.getOnClickMsg() + selectedPlayerName + ".");
-							gp.getRole().performAbility(selectedPlayerName, gp.getArena().getGame());
-							items.getGUI().getViewers().get(0).closeInventory();
-							break;
-						} else {
-							p.sendMessage("§cYou are not allowed to do this right now!");
-							items.getGUI().getViewers().get(0).closeInventory();
-						}
+				} else {
+					for(GameItem items : gp.getItems()){
+						if(items.getGUI().equals(e.getClickedInventory())){ //check if player is clicking in the GUI designated for its role
+							if(gp.getArena().getGame().getEvent().equals("Night")){  //TODO allow for jailors to do their choosing
+								p.sendMessage(items.getOnClickMsg() + selectedPlayerName + ".");
+								gp.getRole().performAbility(selectedPlayerName, gp.getArena().getGame());
+								items.getGUI().getViewers().get(0).closeInventory();
+								break;
+							} else {
+								p.sendMessage("§cYou are not allowed to do this right now!");
+								items.getGUI().getViewers().get(0).closeInventory();
+							}
 
+						}
 					}
 				}
 			}
